b79a21207f50a934e43f6e89e9d0ab327cdbf85b
eugene <charles1304@daum.net>
2023-03-20 17:37:42 +0900

    MY COMMIT 4 이진화 기능 추가

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import cv2
 
 # 영상소스 열기
 
-src = cv2.imread('c:/photo/picture11.jpg')
+src = cv2.imread('c:/photo/picture88.jpg')
 
 
 
 ## 영상 처리 /컴퓨터 비전 알고리즘 적용 ##
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-
+dst3 = src.copy()
+_, dst3 = cv2.threshold(dst3, 127, 255, cv2.THRESH_BINARY)
 
 
 
@@ -20,6 +21,7 @@ dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 cv2.imshow('src',src)
 cv2.imshow('dst1',dst1)
 cv2.imshow('dst2',dst2)
+cv2.imshow('dst3',dst3)
 
 # 마무리
 cv2.waitKey(0)
