25862dbaf678cb83ff34a3f6a60c584a87fb3bd6
eugene <charles1304@daum.net>
2023-03-20 17:31:02 +0900

    my commit 2 그레이스 케일 변환 추가

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ src = cv2.imread('c:/photo/picture01.jpg')
 
 
 
+## 영상 처리 /컴퓨터 비전 알고리즘 적용 ##
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 
 
 
@@ -16,11 +18,9 @@ src = cv2.imread('c:/photo/picture01.jpg')
 # 영상 디스플레이
 
 cv2.imshow('src',src)
-
+cv2.imshow('dst1',dst1)
 
 
 # 마무리
-
-
 cv2.waitKey(0)
 cv2.destroyAllWindow()
